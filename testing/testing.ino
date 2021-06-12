6581abec122a7a2b7652384dff3c1d827058a911
amber-chu <85570852+amber-chu@users.noreply.github.com>
2021-06-12 17:49:44 -0500

    display text

--- testing/testing.ino
+++ testing/testing.ino
@@ -181,4 +181,11 @@ void loop() {
     pulse(strip.Color(0, 255 ,255), 50); //aqua
     pulse(strip.Color(30, 0 ,255), 50); //blue
     pulse(strip.Color(255, 0 ,151), 50); //pink
+
+    u8g2.clearBuffer();
+    //u8g2.setFont(u8g2_font_ncenBOO_tr);
+    u8g2.drawStr(0,10, "heyoo nerd");
+    u8g2.sendBuffer();
+    delay(5000);
+    
     }
