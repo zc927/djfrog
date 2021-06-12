db07e2d9f033e85cf235751f75db5e7272ae4a79
amber-chu <85570852+amber-chu@users.noreply.github.com>
2021-06-11 22:41:57 -0500

    keyboard stuff

--- testing/testing.ino
+++ testing/testing.ino
@@ -1,3 +1,5 @@
+#include <Keyboard.h>
+
 //hey amber!! heres the basic setups:
 
 #include <U8g2lib.h>
@@ -6,6 +8,7 @@
 #include "LIS3DHTR.h"
 LIS3DHTR<TwoWire> LIS;
 #define WIRE Wire
+#include "Adafruit_NeoPixel.h"
 
 #define DHTPIN 3
 #define DHTTYPE DHT11
@@ -23,6 +26,7 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R2, SCL, SDA, U8X8_PIN_NONE);
 #define screen_width 128
 #define screen_height 64
 const int fps = 0; //fast as possible b/c sensors make it slow
+const int currentFrame = 0;
 
 /*const int display_width = 164;
 int current_pos = -display_width;
@@ -40,8 +44,36 @@ void setup() {
   u8g2.begin();
   pinMode(button, INPUT);
   pinMode(led, OUTPUT);
+  Keyboard.begin();
 
   tick.start(fps, AsyncDelay::MILLIS);
+}
+
+void duckAnimate() {
+  u8g2.firstPage();
+  do {
+    switch (currentFrame) {
+      case 0:
+        //u8g2.drawXBMP(screen_width / 2,0, screen_width, screen_height, frame1);
+        break;
+      case 1: 
+        //u8g2.drawXBMP(screen_width / 2,0, screen_width, screen_height, frame2);
+        break;
+    }
+  }
+  while(u8g2.nextPage());
+}
+
+void changeSong()
+{
+  Keyboard.press(KEY_LEFT_GUI);
+  delay(100);
+  Keyboard.release();
+}
+
+void pulse()
+{
+  //insert
 }
 
 void loop() { 
@@ -67,29 +99,6 @@ void loop() {
       }
       
     tick.repeat();
-}
-
-void duckAnimate() {
-  u8g2.firstPage();
-  do {
-    switch (animate_frame) {
-      case 0:
-        u8g2.drawXBMP(current_pos,0, screen_width, screen_height, frame1);
-        break;
-      case 1: 
-        u8g2.drawXBMP(current_pos,0, screen_width, screen_height, frame2);
-        break;
-    }
-  }
-  while(u8g2.nextPage());
-}
-
-void changeSong();
-{
-}
-
-void pulse();
-{
 }
 
 }
